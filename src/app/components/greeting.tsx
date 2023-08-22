34576e09c760f7dd22039f344c53f7d6c0bfa53a
Fatim <Fatim@Taxation5.dhmc.com.pk>
2023-08-22 10:28:36 +0500

    changes

--- src/app/components/greeting.tsx
+++ src/app/components/greeting.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
 const Greeting = () => {
-    let curDate=new Date()
-    curDate=curDate.getHours()
+    const curDate=new Date()
+    const hours=curDate.getHours()
     let greetings="";
     const cssStyle={};
-    if(curDate>=1 && curDate<12){
+    if(hours>=1 && hours<12){
         greetings="Good Morning"
         cssStyle.color="green"
     }else if
-    (curDate>=12 && curDate<17){
+    (hours>=12 && hours<17){
         greetings="Good Afternoon"
         cssStyle.color="orange"
     }
     else if
-    (curDate>=17 && curDate<19){
+    (hours>=17 && hours<19){
         greetings="Good Evening"
         cssStyle.color="purple"}
     else{
